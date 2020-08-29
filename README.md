27dac84253b048575833f55759bc2dcb69654fb6
ArashAnwar <67472314+ArashAnwar@users.noreply.github.com>
2020-08-29 10:53:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# STREET-FIGHTER
+# staticSquare
+A static square made using Matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,13 @@
+var Player;
+
+function setup(){
+createCanvas(displayWidth,displayHeight);
+Player = createSprite(400,200,50,60)
+
+}
+
+function draw(){
+
+    background(0);
+    drawSprites();
+}
